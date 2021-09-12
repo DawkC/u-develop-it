6c4e288bbc0311ac992b144506ac34405d97e3ed
Chuck D <c.a.dawkins@outlook.com>
2021-09-11 20:53:06 -0400

    final changes

--- db/connection.js
+++ db/connection.js
@@ -6,7 +6,7 @@ const db = mysql.createConnection({
     // Your MySQL username,
     user: 'root',
     // Your MySQL password
-    password: 'beemwu318I$',
+    password: 'password',
     database: 'election'
 });
 
